6463c93235cf88980e0678f5ad772dc111590dd7
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-12-13 21:45:47 -0300

    A

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 rem ng build --output-path dist --base-href './'.
-ng build --output-path dist --base-href './dist'.
+ng build --output-path dist --base-href '/dist'.
 echo "Copiando arquivo"
 copy redirect_index.php dist\index.php
