f1ea3f4bcb9e381a45679767694e2fc6166213de
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-12-20 16:14:46 -0300

    A

--- src/app/020_brapci/widget/citation-note/citation-note.component.ts
+++ src/app/020_brapci/widget/citation-note/citation-note.component.ts
@@ -8,7 +8,32 @@ import { Component, Input } from '@angular/core';
 export class CitationNoteComponent {
   @Input() public citacao: Array<any> | any;
 
+  copiarTextoID(id: string): void {
+    // Busca o elemento pelo ID
+    const elemento = document.getElementById(id);
+
+    if (elemento) {
+      // Obtém o conteúdo da div como texto puro
+      const conteudo = elemento.innerText;
+
+      // Copia o texto para o clipboard
+      navigator.clipboard
+        .writeText(conteudo)
+        .then(() => {
+          alert('Conteúdo copiado com sucesso!');
+        })
+        .catch((err) => {
+          console.error('Erro ao copiar o texto: ', err);
+        });
+    } else {
+      console.error(`Elemento com o ID "${id}" não foi encontrado.`);
+    }
+  }
+
   copiarTexto(conteudo: string) {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = conteudo; // Coloca o HTML
+    const textoPuro = tempElement.innerText || tempElement.textContent || ''; // Remove as tags HTML
     navigator.clipboard
       .writeText(conteudo)
       .then(() => {
