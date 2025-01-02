8c14e4785545273565804941703ee47110767afb
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-02 19:26:26 -0300

    A

---
+++ src/app/020_brapci/widget/resume/resume.component.ts
@@ -0,0 +1,19 @@
+import { BrapciService } from './../../../010_service/brapci.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume',
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.scss'
+})
+export class ResumeComponent {
+  constructor(private brapciService: BrapciService) { }
+  data: any;
+  ngOnInit() {
+      let url = 'brapci/statistics';
+      this.brapciService.api_post(url).subscribe((res) => {
+        this.data = res;
+        console.log(this.data);
+      })
+    }
+}
