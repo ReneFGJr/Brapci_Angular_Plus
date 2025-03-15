d9393a1fa27c68f0b4104338d07e1b0c3f89c5bd
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-03-15 02:59:58 -0300

    A

--- src/app/020_brapci/widget/v/book-summary/book-summary.component.ts
+++ src/app/020_brapci/widget/v/book-summary/book-summary.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { BrapciService } from 'src/app/010_service/brapci.service';
+import { LocalStorageService } from 'src/app/010_service/local-storage.service';
 
 @Component({
   selector: 'app-book-summary',
@@ -8,9 +11,16 @@ import { Component, Input } from '@angular/core';
 export class BookSummaryComponent {
   @Input() data: any;
   chapters: Array<any> = [];
-  constructor() { }
+
+  constructor(
+    private brapciService: BrapciService,
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {}
+
   ngOnInit(): void {
     this.chapters = this.data?.summary || [];
+    this.chapters = this.data?.data?.hasBookChapter || [];
   }
 
 }
